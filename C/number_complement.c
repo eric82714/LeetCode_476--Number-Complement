33e3e8fc8dd828fa9e30b1e410aa1b51d72f0bdf
eric82714 <0958820714ab@gmail.com>
2020-04-30 08:24:48 +0800

    Create number_complement.c

---
+++ C/number_complement.c
@@ -0,0 +1,11 @@
+int findComplement(int num){
+    int result = 0, degree = 0;
+        
+    while(num > 0) {
+        result = pow(2, degree) * (1 - num % 2) + result;
+        degree ++;
+        num = num / 2;            
+    }
+        
+    return result;
+}
